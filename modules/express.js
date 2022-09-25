2ac09404afc1697fc3c4711c67322796b5fdf6c8
rodolformartins <rodolfo.martins@dilettasolutions.com>
2022-09-25 09:58:25 -0300

    include ejs

--- modules/express.js
+++ modules/express.js
@@ -9,6 +9,20 @@ const app = express();
 
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+app.use((req, res, next) => {
+    console.log(`Request Type : ${req.method}`);
+    console.log(`Content Type: ${req.headers["content-type"]}`);
+    console.log(`Date: ${new Date()}`);
+    next();
+})
+
+app.get('/views/users', async (req, res) => {
+    const users = await userMethod.find({})
+    res.render('index', { users });
+})
 
 //Consultando todos os usuários na base de dados
 app.get('/users', async (req, res) => {
@@ -56,6 +70,18 @@ app.patch("/users/:id", async (req, res) => {
         res.status(500).send(error.message)
     }
 });
+
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const user = await userMethod.findByIdAndRemove(id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
 
 const port = 8080;
 
