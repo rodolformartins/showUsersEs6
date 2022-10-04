4ead0f34512a39b877cabb94f80f285d938d9133
rodolformartins <rodolfo.martins@dilettasolutions.com>
2022-10-04 07:48:11 -0300

    change class

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv'
 import { Connect } from './src/controller/connect.js';
-import './src/controller/express.js'
+import { Consultas } from './src/controller/express.js';
 
 
 dotenv.config();
 const conexao = new Connect();
+const consulta = new Consultas();
 conexao.connectToDataBase();
+consulta.consultas();
 
--- src/controller/express.js
+++ src/controller/express.js
@@ -1,89 +1,96 @@
 import express from 'express'
 import { UserModel } from '../models/user.model.js'
 
-const userModel = new UserModel();
+export class Consultas {
 
-const userMethod = userModel.modelReturn();
+    consultas() {
+        const userModel = new UserModel();
 
-const app = express();
+        const userMethod = userModel.modelReturn();
 
-app.use(express.json());
+        const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'src/views');
+        app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(`Request Type : ${req.method}`);
-    console.log(`Content Type: ${req.headers["content-type"]}`);
-    console.log(`Date: ${new Date()}`);
-    next();
-})
+        app.set('view engine', 'ejs');
+        app.set('views', 'src/views');
 
-//endpoint para visualizar users
-app.get('/views/users', async (req, res) => {
-    const users = await userMethod.find({})
-    res.render('index', { users });
-})
+        app.use((req, res, next) => {
+            console.log(`Request Type : ${req.method}`);
+            console.log(`Content Type: ${req.headers["content-type"]}`);
+            console.log(`Date: ${new Date()}`);
+            next();
+        })
 
-//Consultando todos os usuários na base de dados
-app.get('/users', async (req, res) => {
-    try {
-        const users = await userMethod.find({});
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).send(error.message);
+        //endpoint para visualizar users
+        app.get('/views/users', async (req, res) => {
+            const users = await userMethod.find({})
+            res.render('index', { users });
+        })
+
+        //Consultando todos os usuários na base de dados
+        app.get('/users', async (req, res) => {
+            try {
+                const users = await userMethod.find({});
+                res.status(200).json(users);
+            } catch (error) {
+                res.status(500).send(error.message);
+            }
+        });
+
+
+        //consultando usuário por ID
+        app.get('/users/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const user = await userMethod.findById(id);
+
+                return res.status(200).json(user);
+            } catch (error) {
+                return res.status(500).send(error.message)
+            }
+        });
+
+
+
+        app.post('/users', async (req, res) => {
+            try {
+                const user = await userMethod.create(req.body);
+
+                res.status(201).json(user);
+            } catch (error) {
+                res.status(500).send(error.message);
+            }
+        })
+
+        // Atualizar um campo especifico de algum usuário
+        app.patch("/users/:id", async (req, res) => {
+            try {
+                const id = req.params.id;
+                const user = await userMethod.findByIdAndUpdate(id, req.body, { new: true });
+
+                res.status(200).json(user);
+            } catch (error) {
+                res.status(500).send(error.message)
+            }
+        });
+
+        app.delete("/users/:id", async (req, res) => {
+            try {
+                const id = req.params.id;
+
+                const user = await userMethod.findByIdAndRemove(id);
+
+                res.status(200).json(user);
+            } catch (error) {
+                res.status(500).send(error.message);
+            }
+        })
+
+        const port = 8080;
+
+        app.listen(port, () => console.log(`Rodando com expressa na porta ${port}!`));
     }
-});
 
+}
 
-//consultando usuário por ID
-app.get('/users/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await userMethod.findById(id);
-
-        return res.status(200).json(user);
-    } catch (error) {
-        return res.status(500).send(error.message)
-    }
-});
-
-
-
-app.post('/users', async (req, res) => {
-    try {
-        const user = await userMethod.create(req.body);
-
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-})
-
-// Atualizar um campo especifico de algum usuário
-app.patch("/users/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await userMethod.findByIdAndUpdate(id, req.body, { new: true });
-
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-});
-
-app.delete("/users/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-
-        const user = await userMethod.findByIdAndRemove(id);
-
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-})
-
-const port = 8080;
-
-app.listen(port, () => console.log(`Rodando com expressa na porta ${port}!`));
